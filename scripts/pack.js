c21b7fbd5cd25babf20f01a6b5eea580c65bc9dc
richard chen <cdchenliqiang@jd.com>
2016-09-15 13:57:24 +0800

    add pack

--- scripts/pack.js
+++ scripts/pack.js
@@ -3,12 +3,13 @@ const packager = require('electron-packager')
 packager({
     dir: './dist',
     out: './out',
-    all: true,
+    arch: ['ia32', 'x64'],
+    platform: ['linux', 'win32', 'darwin'],
     download: 'https://npm.taobao.org/mirrors/electron'
 }, function(err, appPaths) {
     if(err) {
         console.log('[error] ' + err)
     } else {
-        console.log('app published to ' + appPaths)
+        console.log('[success] ' + appPaths)
     }
 })
