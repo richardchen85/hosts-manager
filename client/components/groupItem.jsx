eacae8e497edfcf360846005c9a9272c6d63b225
陈礼强 <cdchenliqiang@jd.com>
2019-03-20 17:41:19 +0800

    ui修改

--- client/components/groupItem.jsx
+++ client/components/groupItem.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react'
 import CodeArea from './codeArea'
+import Modal from './modal/modal'
 
 function Header(props) {
-    let { group, active, editing, index, toggleExpand, changeActive, changeStatus, handleDelete } = props
+    let { group, active, editing, index, toggleExpand, changeActive, changeStatus, onDelete } = props
     return (
         <header className="header">
             <div className="arrow" onClick={ e => toggleExpand(index, !group.get('expand'))}/>
             <h3 className="title">
                 <span onClick={ e => changeActive(!active, index) }>
+                    <em>{group.get('groupName')}</em>
                     <i className="iconfont i-no-check">&#xe604;</i>
                     <i className="iconfont i-checked">&#xe605;</i>
-                    <em>{group.get('groupName')}</em>
                 </span>
             </h3>
             <div className="ctrl">
                 <i className="iconfont i-edit" title="modify"
                     onClick={ e => changeStatus(!editing, index) }>&#xe606;</i>
                 <i className="iconfont i-del" title="delete"
-                    onClick={e => handleDelete(index)}>&#xe600;</i>
+                    onClick={e => onDelete(index)}>&#xe600;</i>
             </div>
         </header>
     )
@@ -46,12 +47,19 @@ export default class GroupItem extends Component {
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.handleDelete = this.handleDelete.bind(this)
     }
 
     handleChange(content) {
         this.setState({
             content
         })
+    }
+
+    handleDelete(index) {
+        if (window.confirm('Are you sure to delete?')) {
+            this.props.handleDelete(index);
+        }
     }
 
     render() {
@@ -69,9 +77,9 @@ export default class GroupItem extends Component {
 
         return (
             <li className={className.join(' ')}>
-                <Header {...this.props} active={active} editing={editing} />
+                <Header {...this.props} active={active} editing={editing} onDelete={this.handleDelete} />
                 <div className="code-wrap">
-                    <CodeArea value={content} readonly={editing} onChange={this.handleChange} />
+                    <CodeArea value={content} readonly={!editing} onChange={this.handleChange} />
                 </div>
                 <Footer {...this.props} content={content} />
             </li>
